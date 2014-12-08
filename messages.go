a7e85f4c7064654fd24266210f3757205b4e3293
mulargui <mauricio@ulargui.com>
2014-12-07 19:58:08 -0800

    included 2 rest apis

--- messages.go
+++ messages.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"net/http"
-	"bytes"
+	//"bytes"
 	"fmt"
-	//"encoding/json"
+	"encoding/json"
     "database/sql"
     _ "github.com/go-sql-driver/mysql"
 )
@@ -15,43 +15,150 @@ func taxonomy(response http.ResponseWriter, r *http.Request) {
 		response.WriteHeader(http.StatusNotAcceptable)
 		return
 	}
-	
 	var user="root"
 	var password="awsawsdb"
 	var database="healthylinkx"
 	con, err := sql.Open("mysql", user+":"+password+"@/"+database)
-	if err != nil { /*error*/}
-
+	if err != nil { 
+		response.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	
 	defer con.Close()
 
 	rows, err := con.Query("SELECT * FROM taxonomy")
-	if err != nil { /*error*/}
+	if err != nil { 
+		response.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
 	
-	var buffer bytes.Buffer
-	var taxonomy string
+	type Speciality struct {
+		Classification string
+	}
+	var MyList [] Speciality
+	var taxonomy Speciality
+
 	for rows.Next() {
-		err = rows.Scan(&taxonomy)
+		err = rows.Scan(&taxonomy.Classification)
 		if err == nil {
-				buffer.WriteString(taxonomy)
+			MyList = append(MyList,taxonomy)
 		}
-		fmt.Fprintf(response, buffer. String())
 	}
 		
-	fmt.Fprintf(response, "Hello taxonomy!")
+	b, err := json.Marshal(MyList)
+	if err != nil { 
+		response.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusOK)
+	response.Write(b)
+}
+
+func shortlist(response http.ResponseWriter, r *http.Request) {
+	//Only GET methods
+	if r.Method != "GET" {
+		response.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	
+	var params = r.URL.Query()
+	var npi1 = params.Get("NPI1")
+	var npi2 = params.Get("NPI2")
+	var npi3 = params.Get("NPI3")
+
+ 	//check params
+ 	if len(npi1)==0 {
+		response.WriteHeader(http.StatusNoContent)
+		return
+ 	}
+
+	var user="root"
+	var password="awsawsdb"
+	var database="healthylinkx"
+	con, err := sql.Open("mysql", user+":"+password+"@/"+database)
+	if err != nil { 
+		response.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	defer con.Close()
+
+	transaction, err := con.Exec("INSERT INTO transactions VALUES (DEFAULT,DEFAULT,?,?,?)", npi1,npi2,npi3)
+	if err != nil { 
+		response.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	
+	transactionid,err := transaction.LastInsertId()
+	if err != nil { 
+		response.WriteHeader(http.StatusNotAcceptable)
+		return
+	}	
+	
+	type provider struct {
+		NPI string
+		Provider_Full_Name string
+		Provider_Full_Street string
+		Provider_Full_City string
+		Provider_Business_Practice_Location_Address_Telephone_Number string
+	}
+	type list struct {
+		transaction int64
+		providers [] provider
+	}
+	var MyList list
+	MyList.transaction=transactionid
+
+	//return detailed data of the selected providers
+	var querystring = "SELECT NPI,Provider_Full_Name,Provider_Full_Street, Provider_Full_City, Provider_Business_Practice_Location_Address_Telephone_Number FROM npidata2 WHERE ((NPI = '"+ npi1 +"')"
+ 	if len(npi2)!=0 {
+		querystring += "OR (NPI = '" + npi2 +"')"
+	}
+ 	if len(npi3)!=0 {
+		querystring += "OR (NPI = '" + npi3 +"')"
+	}
+	querystring += ")";
+	
+
+	rows, err := con.Query(querystring)
+	if err != nil { 
+		response.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	var item provider
+	for rows.Next() {
+		err = rows.Scan(&(item.NPI), 
+			&(item.Provider_Full_Name), 
+			&(item.Provider_Full_Street), 
+			&(item.Provider_Full_City), 
+			&(item.Provider_Business_Practice_Location_Address_Telephone_Number))
+		if err == nil {
+			MyList.providers = append(MyList.providers,item)
+		}
+	}
 
 /*	
-	$sql = mysql_query($query, $this->db);
-
-	if(mysql_num_rows($sql) <= 0)
-		$this->response('no taxonomy records',204); //If no records "No Content" status
-		
-	$result = array();
-	while($rlt = mysql_fetch_array($sql,MYSQL_ASSOC))
-		$result[] = $rlt;
-
-	// If success everythig is good send header as "OK" and return list of specialities in JSON format
-	$this->response($this->json($result), 200);
+	fmt.Fprintf(response, "----#%d#%s#%s#%s#%s#%s#----",
+		MyList.transaction,
+		MyList.providers[2].NPI, 
+		MyList.providers[2].Provider_Full_Name, 
+		MyList.providers[2].Provider_Full_Street, 
+		MyList.providers[2].Provider_Full_City,
+		MyList.providers[2].Provider_Business_Practice_Location_Address_Telephone_Number)
+	fmt.Fprintf(response, "step11")
+	return
 */
+	
+	b, err := json.Marshal(MyList)
+	if err != nil { 
+		response.WriteHeader(http.StatusNotAcceptable)
+		return
+	}
+	
+	//response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusOK)
+	response.Write(b)
 }
 
 func providers(response http.ResponseWriter, r *http.Request) {
@@ -62,7 +169,6 @@ func providers(response http.ResponseWriter, r *http.Request) {
 	}
 	
 	var params = r.URL.Query()
-	
 	var gender = params.Get("gender")
 	var lastname1 = params.Get("lastname1")
 	var lastname2 = params.Get("lastname2")
@@ -73,17 +179,12 @@ func providers(response http.ResponseWriter, r *http.Request) {
 
  	//check params
  	if len(zipcode)==0 && len(lastname1)==0 && len(specialty)==0 {
-		//response.WriteHeader(http.StatusNoContent)
-		fmt.Fprintf(response,"not enough parameters")
+		response.WriteHeader(http.StatusNoContent)
 		return
  	}
 
 	//building the query string
 	fmt.Fprintf(response, "Hello providers! #%s#%s#%s#%s#%s#%s#%s#", gender, lastname1,lastname2,lastname3,specialty,distance,zipcode)
-}
-
-func shortlist(response http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(response, "Hello shortlist!")
 }
 
 func transaction(response http.ResponseWriter, r *http.Request) {
